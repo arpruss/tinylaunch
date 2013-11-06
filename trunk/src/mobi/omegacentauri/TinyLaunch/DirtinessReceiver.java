e8315f887a71342cc1f1096cf505cdfde96be59d
arpruss <arpruss@f2e9a530-7954-1603-4467-2a174b1cf3e9>
2013-11-06 05:13:53 +0000

    dirtiness

---
+++ trunk/src/mobi/omegacentauri/TinyLaunch/DirtinessReceiver.java
@@ -0,0 +1,19 @@
+package mobi.omegacentauri.TinyLaunch;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class DirtinessReceiver extends BroadcastReceiver {
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		SharedPreferences options = PreferenceManager.getDefaultSharedPreferences(context);
+		options.edit().putBoolean(Options.PREF_DIRTY, true).commit();
+		Log.v("TinyLaunch", "marked as dirty");
+	}
+}
